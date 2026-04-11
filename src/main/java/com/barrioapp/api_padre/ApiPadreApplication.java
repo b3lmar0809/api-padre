0f606355fd8ee5bf67b247c24570f87afad1db40
belmar <mati.belmar0625@gmail.com>
2026-04-11 18:38:36 -0400

    feat: estructura inicial api-padre

---
+++ src/main/java/com/barrioapp/api_padre/ApiPadreApplication.java
@@ -0,0 +1,13 @@
+package com.barrioapp.api_padre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPadreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPadreApplication.class, args);
+	}
+
+}
