c34435accefd06193a0341f1bca9a24e95677f2a
belmar <mati.belmar0625@gmail.com>
2026-04-16 16:25:40 -0400

    chore: cambiando cosas de español a ingles

--- src/main/java/com/barrioapp/api_padre/service/serviceImpl/PlanServiceImpl.java
+++ src/main/java/com/barrioapp/api_padre/service/serviceImpl/PlanServiceImpl.java
@@ -40,17 +40,17 @@ public class PlanServiceImpl implements PlanService {
     @Override
     public PlanResponse getPlan(Long id) {
         Plan plan = planRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Plan no encontrado"));
+                .orElseThrow(() -> new RuntimeException("Plan not found"));
         return new PlanResponse(plan.getId(), plan.getType().name(), plan.getLimitProduct(), plan.getPrice());
     }
 
     @Override
     public UserResponse changePlan(Long userId, Long planId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                .orElseThrow(() -> new RuntimeException("User not found"));
 
         Plan plan = planRepository.findById(planId)
-                .orElseThrow(() -> new RuntimeException("Plan no encontrado"));
+                .orElseThrow(() -> new RuntimeException("Plan not found"));
 
         user.setPlan(plan);
         userRepository.save(user);
@@ -58,7 +58,7 @@ public class PlanServiceImpl implements PlanService {
         String token = jwtService.generateToken(user);
         sessionService.saveSession(user.getId(), token);
 
-        return new UserResponse(user.getId(), user.getName(), user.getEmail(),
+        return new UserResponse(user.getId(), user.getName(), user.getLastName(), user.getEmail(),
                 user.getCompanyName(), plan.getType().name(), token);
     }
 }
