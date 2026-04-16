ce9170a2fc7d54e78092f9f7925afdfe38937813
belmar <mati.belmar0625@gmail.com>
2026-04-16 16:14:45 -0400

    add: enum finance

---
+++ src/main/java/com/barrioapp/api_padre/model/MovementType.java
@@ -0,0 +1,14 @@
+package com.barrioapp.api_padre.model;
+
+/**
+ * MovementType class
+ *
+ * @Version: 1.0.0 - 14 abr. 2026
+ * @Author: Matias Belmar - mati.belmar0625@gmail.com
+ * @Since: 1.0.0 - 2026/04/14
+ */
+
+public enum MovementType {
+    INCOME,
+    EXPENSE
+}
