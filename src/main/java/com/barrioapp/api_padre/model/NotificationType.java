3d7e9254704699071752827f33683af05b637320
belmar <mati.belmar0625@gmail.com>
2026-04-18 16:22:21 -0400

    add: enum notification

---
+++ src/main/java/com/barrioapp/api_padre/model/NotificationType.java
@@ -0,0 +1,15 @@
+package com.barrioapp.api_padre.model;
+
+/**
+ * NotificationType class
+ *
+ * @Version: 1.0.0 - 16 abr. 2026
+ * @Author: Matias Belmar - mati.belmar0625@gmail.com
+ * @Since: 1.0.0 - 2026/04/16
+ */
+
+public enum NotificationType {
+    WELCOME,
+    LOW_STOCK,
+    UPDATE_PLAN
+}
