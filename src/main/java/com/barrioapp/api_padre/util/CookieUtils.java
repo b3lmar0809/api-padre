0135ff48bc42ab340a6f6fe5b4a3a2693d795696
belmar <mati.belmar0625@gmail.com>
2026-04-20 21:15:05 -0400

    add: CookieUtils for HTTP cookie management

---
+++ src/main/java/com/barrioapp/api_padre/util/CookieUtils.java
@@ -0,0 +1,39 @@
+package com.barrioapp.api_padre.util;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+
+/**
+ * CookieUtils class
+ *
+ * @Version: 1.0.0 - 18 abr. 2026
+ * @Author: Matias Belmar - mati.belmar0625@gmail.com
+ * @Since: 1.0.0 - 18 abr. 2026
+ */
+public class CookieUtils {
+
+    private CookieUtils() {}
+
+    public static void addTokenCookie(HttpServletResponse response, String token) {
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(86400)
+                .sameSite("Strict")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+
+    public static void clearTokenCookie(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("token", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+}
